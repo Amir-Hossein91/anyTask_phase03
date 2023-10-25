89fce929c07bb1dd97cdec24df85d43b18f946be
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-25 16:57:34 +0330

    phase 3: created controller classes

---
+++ src/main/java/com/example/phase_03/controller/TechnicianSuggestionController.java
@@ -0,0 +1,11 @@
+package com.example.phase_03.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("technicianSuggestion")
+public class TechnicianSuggestionController {
+
+
+}
