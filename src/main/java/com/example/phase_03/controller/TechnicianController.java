1f625522035b4a8e0419122dd176db873ab86de5
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-26 02:35:03 +0330

    phase 3: created /save endpoint in TechnicianController

--- src/main/java/com/example/phase_03/controller/TechnicianController.java
+++ src/main/java/com/example/phase_03/controller/TechnicianController.java
@@ -1,9 +1,41 @@
 package com.example.phase_03.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.phase_03.dto.request.TechnicianRequestDTO;
+import com.example.phase_03.dto.response.TechnicianResponseDTO;
+import com.example.phase_03.entity.Technician;
+import com.example.phase_03.entity.enums.TechnicianStatus;
+import com.example.phase_03.mapper.TechnicianMapper;
+import com.example.phase_03.service.impl.TechnicianServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/technician")
 public class TechnicianController {
+
+    @Autowired
+    TechnicianServiceImpl technicianService;
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<TechnicianResponseDTO> saveTechnician (@RequestBody TechnicianRequestDTO requestDTO) throws IOException {
+        Technician technician = TechnicianMapper.INSTANCe.dtoToModel(requestDTO);
+
+        byte[] image = technician.getImage();
+        Path path = Path.of("C:\\Users\\AmirHossein\\IdeaProjects\\anyTask\\image_output\\technician_01.jpg");
+        Files.write(path,image);
+
+        technician.setTechnicianStatus(TechnicianStatus.NEW);
+
+        technicianService.saveOrUpdate(technician);
+        return new ResponseEntity<>(TechnicianMapper.INSTANCe.modelToDto(technician), HttpStatus.CREATED);
+    }
 }
