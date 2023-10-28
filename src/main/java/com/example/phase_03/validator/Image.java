4bf37b9acf995e58d1171c7d0beda99f8acdd78e
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-28 16:55:27 +0330

    phase 3: removed unnecessary fields from @Image

--- src/main/java/com/example/phase_03/validator/Image.java
+++ src/main/java/com/example/phase_03/validator/Image.java
@@ -2,7 +2,6 @@ package com.example.phase_03.validator;
 
 
 import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -15,8 +14,4 @@ import java.lang.annotation.Target;
 public @interface Image {
 
     String message() default "";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
 }
