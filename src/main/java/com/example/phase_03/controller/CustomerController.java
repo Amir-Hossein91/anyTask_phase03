083d791e4b7bbb5275db813dc21672152e7a3e04
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-29 17:44:39 +0330

    phase 3: methods for customer to see the list of subAssistances and to make an order are created

--- src/main/java/com/example/phase_03/controller/CustomerController.java
+++ src/main/java/com/example/phase_03/controller/CustomerController.java
@@ -1,20 +1,26 @@
 package com.example.phase_03.controller;
 
 import com.example.phase_03.dto.request.CustomerRequestDTO;
+import com.example.phase_03.dto.request.OrderRequestDTO;
 import com.example.phase_03.dto.response.CustomerResponseDTO;
+import com.example.phase_03.dto.response.OrderResponseDTO;
+import com.example.phase_03.dto.response.SubAssistanceResponseDTO;
 import com.example.phase_03.entity.Customer;
+import com.example.phase_03.entity.Order;
+import com.example.phase_03.entity.SubAssistance;
+import com.example.phase_03.entity.enums.OrderStatus;
 import com.example.phase_03.mapper.CustomerMapper;
-import com.example.phase_03.service.impl.CustomerServiceImpl;
-import com.example.phase_03.service.impl.PersonServiceImpl;
+import com.example.phase_03.mapper.OrderMapper;
+import com.example.phase_03.mapper.SubAssistanceMapper;
+import com.example.phase_03.service.impl.*;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -23,11 +29,20 @@ public class CustomerController {
 
     private final CustomerServiceImpl customerService;
     private final PersonServiceImpl personService;
+    private final SubAssistanceServiceImpl subAssistanceService;
+    private final AssistanceServiceImpl assistanceService;
+    private final OrderServiceImpl orderService;
 
     public CustomerController(CustomerServiceImpl customerService,
-                              PersonServiceImpl personService) {
+                              PersonServiceImpl personService,
+                              SubAssistanceServiceImpl subAssistanceService,
+                              AssistanceServiceImpl assistanceService,
+                              OrderServiceImpl orderService) {
         this.customerService = customerService;
         this.personService = personService;
+        this.subAssistanceService = subAssistanceService;
+        this.assistanceService = assistanceService;
+        this.orderService = orderService;
     }
 
     @PostMapping("/register")
@@ -39,4 +54,30 @@ public class CustomerController {
         return new ResponseEntity<>(CustomerMapper.INSTANCE.modelToDto(customerService.findById(customer.getId())), HttpStatus.CREATED);
     }
 
+    @GetMapping("/seeSubAssistance/{username}")
+    public ResponseEntity<List<SubAssistanceResponseDTO>> seeSubAssistances(@PathVariable String username){
+        List<SubAssistance> subAssistances = subAssistanceService.showSubAssistancesToOthers(username);
+        List<SubAssistanceResponseDTO> responseDTOS = new ArrayList<>();
+
+        for(SubAssistance s: subAssistances)
+            responseDTOS.add(SubAssistanceMapper.INSTANCE.modelToDto(s));
+
+        return new ResponseEntity<>(responseDTOS,HttpStatus.OK);
+    }
+
+    @PostMapping("/makeOrder")
+    public ResponseEntity<OrderResponseDTO> makeOrder (@RequestBody @Valid OrderRequestDTO requestDTO) {
+
+        Order order = OrderMapper.INSTANCE.dtoToModel(requestDTO);
+        order.setTechnicianScore(1);
+        order.setSubAssistance(subAssistanceService.findSubAssistance(requestDTO.subAssistanceTitle(), assistanceService.findAssistance(requestDTO.assistanceTitle())));
+        order.setCustomer(customerService.findByUsername(requestDTO.customerUsername()));
+        order.setOrderRegistrationDateAndTime(LocalDateTime.now());
+        order.setOrderStatus(OrderStatus.WAITING_FOR_TECHNICIANS_SUGGESTIONS);
+
+        order = orderService.makeOrder(order.getCustomer().getUsername(),order.getSubAssistance().getTitle()
+                    ,order.getSubAssistance().getAssistance().getTitle(),order.getOrderDescription());
+
+        return new ResponseEntity<>(OrderMapper.INSTANCE.modelToDto(order),HttpStatus.CREATED);
+    }
 }
