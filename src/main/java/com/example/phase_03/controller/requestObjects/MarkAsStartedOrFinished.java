e0157ef69a7020b1e05d6886983b4befc7c107a4
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-30 15:04:23 +0330

    phase 3: refactored to MarkAsStartedOrFinished

--- src/main/java/com/example/phase_03/controller/requestObjects/MarkAsStartedOrFinished.java
+++ src/main/java/com/example/phase_03/controller/requestObjects/MarkAsStartedOrFinished.java
@@ -3,7 +3,7 @@ package com.example.phase_03.controller.requestObjects;
 import lombok.Getter;
 
 @Getter
-public class MarkAsStarted {
+public class MarkAsStartedOrFinished {
 
     private String customerUsername;
     private long orderId;
