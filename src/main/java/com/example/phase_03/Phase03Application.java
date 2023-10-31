26ecbe53c4b79a1957c29fa103e9e308561135cc
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-31 03:37:51 +0330

    phase 3: application starts in headless = false mode

--- src/main/java/com/example/phase_03/Phase03Application.java
+++ src/main/java/com/example/phase_03/Phase03Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Phase03Application {
+    static {
+        System.setProperty("java.awt.headless", "false");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Phase03Application.class, args);
