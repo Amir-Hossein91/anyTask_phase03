ff6f49be25b70f1a19fd51ae27846905ffc15e8d
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-11-02 20:18:59 +0330

    phase 3: deleted unwanted controllers

--- src/main/java/com/example/phase_03/controller/OrderDescriptionController.java
+++
@@ -1,9 +0,0 @@
-package com.example.phase_03.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/orderDescription")
-public class OrderDescriptionController {
-}
