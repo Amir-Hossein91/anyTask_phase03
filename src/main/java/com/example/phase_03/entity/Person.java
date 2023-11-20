6a0b11cd6396827d659ada093d4f8895c753cb18
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-11-20 16:40:19 +0330

    phase 4: deleted validations from Entities

--- src/main/java/com/example/phase_03/entity/Person.java
+++ src/main/java/com/example/phase_03/entity/Person.java
@@ -2,8 +2,6 @@ package com.example.phase_03.entity;
 
 import com.example.phase_03.entity.base.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -22,23 +20,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @SuperBuilder
 public class Person extends BaseEntity {
-    @Pattern(regexp = "^[^\\d]{3,}$", message = "first name should be at least three characters and " +
-            "no digits are allowed")
     @Column(name = "first_name")
     private String firstName;
-    @Pattern(regexp = "^[^\\d]{3,}$", message = "last name should be at least three characters and " +
-            "no digits are allowed")
     @Column(name = "last_name")
     private String lastName;
     @Column(unique = true)
-    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Invalid email address format")
     private String email;
     @Column(unique = true)
-    @NotNull(message = "Username can not be null")
-    @Pattern(regexp = "^[^\\s]+$", message = "Username can not be empty")
     private String username;
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "Password must be exactly " +
-            "8 characters containing digits and letters")
     private String password;
     @Column(name = "registration_date")
     private LocalDateTime registrationDate;
--- src/main/java/com/example/phase_03/entity/SubAssistance.java
+++ src/main/java/com/example/phase_03/entity/SubAssistance.java
@@ -2,10 +2,8 @@ package com.example.phase_03.entity;
 
 import com.example.phase_03.entity.base.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
@@ -18,9 +16,7 @@ import java.util.List;
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 public class SubAssistance extends BaseEntity {
-    @NotNull(message = "Sub-assistance title can not be null")
     private String title;
-    @Range(min = 0, message = "Base price can not be negative")
     @Column(name = "base_Price")
     private long basePrice;
     @ManyToMany
@@ -29,7 +25,6 @@ public class SubAssistance extends BaseEntity {
     private Assistance assistance;
     @OneToMany(mappedBy = "subAssistance")
     private List<Order> orders;
-    @NotNull(message = "Sub-assistance should have some descriptions")
     private String about;
 
     public String toString() {
